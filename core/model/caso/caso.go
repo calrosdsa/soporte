d04790b46e4f7c9c6a1c0c7924683ddd67f939f1
calrosdsa <carlosduram71@gmail.com>
2023-04-20 14:20:15 -0400

    changes

--- core/model/caso/caso.go
+++ core/model/caso/caso.go
@@ -57,7 +57,7 @@ type Caso struct {
 	ClienteApellido        *string    `json:"cliente_apellido"`
 	FuncionarioApellido    *string    `json:"funcionario_apellido,omitempty"`
 	Rol                    *int       `json:"rol"`
-	ProyectoName           *string    `json:"proyecto_namE"`
+	ProyectoName           *string    `json:"proyecto_name"`
 }
 
 type CasoRepository interface {
